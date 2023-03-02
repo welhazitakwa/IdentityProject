5dad3f05965fdd8a47e1099c857cc442484b3233
ChouroukAmara <chouroukamara114@gmail.com>
2023-03-02 10:19:45 +0100

    modification et ajout d'abonnements

--- Project/templates/back/editabonnement.html.php
+++ Project/templates/back/editabonnement.html.php
@@ -1,4 +1,5 @@
-  <!-- Start Breadcrumbbar -->                    
+ <!--
+                    
             <div class="breadcrumbbar">
                 <div class="row align-items-center">
                     <div class="col-md-8 col-lg-8">
@@ -18,13 +19,12 @@
                     </div>
                 </div>          
             </div>
-            <!-- End Breadcrumbbar -->
-            <!-- Start Contentbar -->    
+                
             <div class="contentbar">
               
-                <!-- Start row -->
+                
                 <div class="row justify-content-center">
-                    <!-- Start col -->
+                    
                     <div class="col-lg-12">
                         <div class="card m-b-30">
                             <div class="card-header">
@@ -74,8 +74,65 @@
                             </div>
                         </div>
                     </div>
-                    <!-- End col -->
+                    
                 </div>
-                <!-- End row -->
+                
             </div>
-            <!-- End Contentbar -->
+           
+
+
+
+-->
+
+
+
+
+
+
+
+            <div class="page-wrapper">
+            <div class="content container-fluid">
+               <div class="row">
+                  <div class="col-md-12">
+                     <h5 class="mb-3">Add Abonnements</h5>
+                     <div class="row">
+                        <div class="col-md-6">
+                            <form class="form-validate" method='post' enctype="multipart/form-data">
+                            <input type="hidden" name="abonnement[id]" value="<?=$abonnement->id ?? ''?>">
+                           <div class="form-group form-focus">
+                              <input type="text" class="form-control floating" name="abonnement[titre]" value="<?=$abonnement->titre ?? ''?>" placeholder="Enter title"  required>
+                              <label class="focus-label">Titre <span class="text-danger">*</span></label>
+                           </div>
+
+                           <div class="col-md-6">
+                           <div class="form-group form-focus">
+                              <input type="number" class="form-control floating" name="abonnement[prix]" value="<?=$abonnement->prix ?? ''?>" placeholder="Enter prix"  required>
+                              <label class="focus-label">Prix <span class="text-danger">*</span></label>
+                           </div>
+
+                           <div class="col-md-6">
+                           <div class="form-group form-focus">
+                              <input type="text" class="form-control floating" name="abonnement[duree]" value="<?=$abonnement->duree ?? ''?>" placeholder="Enter Durée"  required>
+                              <label class="focus-label">Durée <span class="text-danger">*</span></label>
+                           </div>
+                          
+                           
+                           <div class="form-group form-focus">
+                              <textarea rows="4" class="form-control bg-grey floating" name="abonnement[avantages]" rows="5" placeholder="Enter Your Avantages." required><?=$abonnement->avantages ?? ''?></textarea>
+                              <label class="focus-label">Avantages <span class="text-danger">*</span></label>
+                           </div>
+                          
+                           <div id="videoId" style="display: none">
+                              <div class="form-group form-focus">
+                                 <input type="text" class="form-control floating">
+                                 <label class="focus-label">Video ID <span class="text-danger">*</span></label>
+                              </div>
+                           </div>
+                           <button type="submit" class="btn btn-primary save-btn">save</button>
+</form>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
