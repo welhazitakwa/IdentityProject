74796563e25fc218cb0bfab2429ce3236a086b2b
welhazitakwa <welhazitakwa@gmail.com>
2023-03-25 11:48:01 +0100

    updatedAt & createdAt in speciallite controller

--- Project/classes/App/Controllers/specialite.php
+++ Project/classes/App/Controllers/specialite.php
@@ -43,48 +43,61 @@ class specialite{
 //--------Enregistrement de données---------//
 
 	public function saveEdit(){
-		
-		/*$specialite = $_POST['specialite'];
-		$this->specialitesTable->save($specialite);
-		header('location: /specialite/list');*/
+			if (isset($_GET['id'])){
+			$dossier="img_spc/";
+			$file=rand(1000,100000)."-".basename($_FILES["fileToUpload"]["name"]);
+			$new_file_name=strtolower($file);		
+			$final_file=str_replace(' ','-',$new_file_name);
+			$target_file=$dossier.$final_file;
+			$file_loc=$_FILES['fileToUpload']['tmp_name'];
+			move_uploaded_file($file_loc,$target_file);
+	if ($_FILES['fileToUpload']['error'] == 4 || ($_FILES['fileToUpload']['size'] == 0 && $_FILES['fileToUpload']['error'] == 0))
+					{
+						$specialite = $_POST['specialite'];
+						$date = date_create(date('Y-m-d')); 
+						$specialite['updatedAt']= date_format($date, 'Y-m-d H:i:s');
+						}
+		else{
+					$specialite = $_POST['specialite'];
+					$specialite['image']=$final_file;
+					$date = date_create(date('Y-m-d')); 
+					$specialite['updatedAt']= date_format($date, 'Y-m-d H:i:s');
+				
+						}
 
+
+			}else{
+			
 		$dossier="img_spc/";
-//Gold 2.jpg
-//Gold 2.jpg
-
-		$file=rand(1000,100000)."-".basename($_FILES["fileToUpload"]["name"]);
-									// 10001-Gold 2.jpg
-		$new_file_name=strtolower($file);		
-			// 10001-gold 2.jpg
-		$final_file=str_replace(' ','-',$new_file_name);
-// 10001-gold-2.jpg
-		
-		$target_file=$dossier.$final_file;
-// img_abn/10001-gold-2.jpg
-		$file_loc=$_FILES['fileToUpload']['tmp_name'];
-//Gold 2.jpg /pc/tmp
-		move_uploaded_file($file_loc,$target_file); // mv tmpfile dossier/nexnamefile
-		
-		
-
-if ($_FILES['fileToUpload']['error'] == 4 || ($_FILES['fileToUpload']['size'] == 0 && $_FILES['fileToUpload']['error'] == 0))
-{	$specialite = $_POST['specialite'];
-    // cover_image is empty (and not an error), or no file was uploaded
-}else{
+			$file=rand(1000,100000)."-".basename($_FILES["fileToUpload"]["name"]);
+			$new_file_name=strtolower($file);		
+			$final_file=str_replace(' ','-',$new_file_name);
+			$target_file=$dossier.$final_file;
+			$file_loc=$_FILES['fileToUpload']['tmp_name'];
+			move_uploaded_file($file_loc,$target_file); // mv tmpfile dossier/nexnamefile
+			
+	if ($_FILES['fileToUpload']['error'] == 4 || ($_FILES['fileToUpload']['size'] == 0 && $_FILES['fileToUpload']['error'] == 0))
+	{
 		$specialite = $_POST['specialite'];
-		$specialite['image']=$final_file;
-}
-
-
-
+		$date = date_create(date('Y-m-d')); 
+		$specialite['createdAt']= date_format($date, 'Y-m-d H:i:s');
+		$specialite['updatedAt']= date_format($date, 'Y-m-d H:i:s');
+		}
+		else{
+			$specialite = $_POST['specialite'];
+			$specialite['image']=$final_file;
+			$date = date_create(date('Y-m-d')); 
+			$specialite['createdAt']= date_format($date, 'Y-m-d H:i:s');
+			$specialite['updatedAt']= date_format($date, 'Y-m-d H:i:s');
 		
-	
-		
-
-		$this->specialitesTable->save($specialite);
-		header('location: /specialite/list');
 	}
+			
 
+		
+		}	
+		 $this->specialitesTable->save($specialite);
+			header('location: /specialite/list');
+	}
 //--------Affichage de données---------//
 
 	public function list(){
